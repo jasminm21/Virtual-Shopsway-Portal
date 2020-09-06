97a31afd5895c90a7c83b4a34f75ca8f7d4cfe93
jasminm21 <itjas.mujawar@gmail.com>
2020-09-06 21:12:43 +0530

    removed product service

--- product-service/src/main/java/com/xp/productservice/dao/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.xp.productservice.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.xp.productservice.model.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Integer>{
-
-}
